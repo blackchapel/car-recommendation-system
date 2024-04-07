df06d3589b3a15e1c6588a4e9a17479085ee3506
blackchapel <kc.dark@protonmail.com>
2024-04-08 01:40:52 +0530

    responsive nav bar

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,33 +1,8 @@
 import React from "react";
-import { Link } from "react-router-dom";
-
-// import Logo from "../assets/images/logo.png";
+import ResponsiveAppBar from "./ResponsiveAppBar";
 
 const Header = () => {
-  return (
-    <nav>
-      <div className="container nav_container">
-        <Link to={"/"} className="nav_logo">
-          <img alt="Navbar Logo" />
-        </Link>
-
-        <ul className="nav_menu">
-          <li>
-            <Link to={"/user"}>Ernest Achiever</Link>
-          </li>
-          <li>
-            <Link to={"/post/create"}>Create Post</Link>
-          </li>
-          <li>
-            <Link to={"/author"}>Authors</Link>
-          </li>
-          <li>
-            <Link to={"/auth/logout"}>Logout</Link>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  );
+  return <ResponsiveAppBar />;
 };
 
 export default Header;
