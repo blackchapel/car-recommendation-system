95f41d5b15843eaba3f5936d101de26a7c6ecfa2
blackchapel <kc.dark@protonmail.com>
2024-04-08 01:40:59 +0530

    Home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,21 @@
 import React from "react";
-
+import { Box } from "@mui/material";
+import { useTheme } from "@emotion/react";
 const Home = () => {
-  return <div></div>;
+  const theme = useTheme();
+  return (
+    <Box
+      style={{
+        backgroundImage: `linear-gradient( rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7) ), url("/assets/car7.jpg")`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        height: `100vh`,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    ></Box>
+  );
 };
 
 export default Home;
