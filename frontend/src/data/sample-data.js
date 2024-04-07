606f74da2e9069fce57c6bd89ebb496230249814
blackchapel <kc.dark@protonmail.com>
2024-04-08 01:45:37 +0530

    sample data

---
+++ frontend/src/data/sample-data.js
@@ -0,0 +1,134 @@
+export const data = [
+  {
+    make: "BMW",
+    model: "Mini Cooper",
+    year: 2018,
+    co2: "200",
+    mileage: "28 mpg",
+    car_type: "ICE", // Electric, ICE, Hybrid
+    drive_train: "front wheel drive", // front-wheel, rear-wheel, all-wheel
+    price: "$45000",
+    image:
+      "https://imgd-ct.aeplcdn.com/370x208/n/cw/ec/39603/cooper-jcw-exterior-right-front-three-quarter-2.jpeg?q=80",
+  },
+  {
+    make: "Toyota",
+    model: "Corolla",
+    year: 2019,
+    co2: "180",
+    mileage: "32 mpg",
+    car_type: "ICE",
+    drive_train: "front wheel drive",
+    price: "$22000",
+    image:
+      "https://www.usnews.com/object/image/0000018c-5f0b-dc6c-aded-ffbf2d960000/https-cars-dms-usnews-com-static-uploads-images-auto-custom-15294-original-2024-toyota-corolla-angular-front-1.jpg?update-time=1693333947000&size=responsive640",
+  },
+  {
+    make: "Honda",
+    model: "Civic",
+    year: 2020,
+    co2: "185",
+    mileage: "31 mpg",
+    car_type: "ICE",
+    drive_train: "front wheel drive",
+    price: "$25000",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/6/6d/2017_Honda_Civic_SR_VTEC_1.0_Front.jpg",
+  },
+  {
+    make: "Ford",
+    model: "Focus",
+    year: 2018,
+    co2: "190",
+    mileage: "30 mpg",
+    car_type: "ICE",
+    drive_train: "front wheel drive",
+    price: "$23000",
+    image:
+      "https://blogs-images.forbes.com/samabuelsamid/files/2017/03/2017-Ford-Focus-RS-15-of-22.jpg",
+  },
+  {
+    make: "Chevrolet",
+    model: "Malibu",
+    year: 2020,
+    co2: "175",
+    mileage: "33 mpg",
+    car_type: "ICE",
+    drive_train: "front wheel drive",
+    price: "$27000",
+    image:
+      "https://www.chevrolet.com/content/dam/chevrolet/na/us/english/index/vehicles/2024/cars/malibu/01-images/mov/2024-malibu-mov-specialeditions-midnightedition-01.png?imwidth=960",
+  },
+  {
+    make: "Volkswagen",
+    model: "Jetta",
+    year: 2019,
+    co2: "180",
+    mileage: "32 mpg",
+    car_type: "ICE",
+    drive_train: "front wheel drive",
+    price: "$24000",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/2/2d/2020_Volkswagen_Jetta_Highline_in_Pure_White%2C_Front_Right%2C_05-21-2022.jpg",
+  },
+  {
+    make: "Hyundai",
+    model: "Elantra",
+    year: 2020,
+    co2: "185",
+    mileage: "31 mpg",
+    car_type: "Electric",
+    drive_train: "front wheel drive",
+    price: "$23000",
+    image:
+      "https://www.rushlane.com/wp-content/uploads/2023/04/hyundai-elantra-n-price.jpg",
+  },
+  {
+    make: "Nissan",
+    model: "Altima",
+    year: 2018,
+    co2: "190",
+    mileage: "30 mpg",
+    car_type: "ICE",
+    drive_train: "front wheel drive",
+    price: "$25000",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/2023_Nissan_Altima_SV_in_Scarlet_Ember_Tintcoat%2C_Front_Left%2C_09-12-2023.jpg/1200px-2023_Nissan_Altima_SV_in_Scarlet_Ember_Tintcoat%2C_Front_Left%2C_09-12-2023.jpg",
+  },
+  {
+    make: "Kia",
+    model: "Forte",
+    year: 2020,
+    co2: "175",
+    mileage: "33 mpg",
+    car_type: "ICE",
+    drive_train: "front wheel drive",
+    price: "$22000",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/6/62/Kia_K3_BD_FL_Snow_White_Pearl_%2813%29_%28cropped%29.jpg",
+  },
+  {
+    make: "Mazda",
+    model: "Mazda3",
+    year: 2019,
+    co2: "180",
+    mileage: "32 mpg",
+    car_type: "Electric",
+    drive_train: "front wheel drive",
+    price: "$26000",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/5/58/2023_Mazda3_Sport_GS_in_Platinum_Quartz_Metallic%2C_Front_Left%2C_10-09-2023.jpg",
+  },
+  {
+    make: "Subaru",
+    model: "Impreza",
+    year: 2018,
+    co2: "190",
+    mileage: "30 mpg",
+    car_type: "Hybrid",
+    drive_train: "all-wheel drive",
+    price: "$28000",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/8/86/04-05_Subaru_WRX_STi_2.jpg",
+  },
+];
