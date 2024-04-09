81800e6334169e9652e9155ba3b8082a4cec3de0
blackchapel <kc.dark@protonmail.com>
2024-04-09 18:40:24 +0530

    improved efficiency

--- backend/main.py
+++ backend/main.py
@@ -228,21 +228,15 @@ async def search_recommendation(index: str = Query(...)):
         similarity = calculate_similarity(user_preference_vector, car_vector)
         car_similarities[index] = {'similarity': similarity}
 
+    print(car_similarities)
     sorted_cars = sorted(car_similarities.items(), key=lambda x: x[1]['similarity'], reverse=True)
 
     cars: list[Car] = []
-    i = 0
-    for car_index, car_info in sorted_cars[:100]:
-        i = i + 1
-
+    for car_index, car_info in sorted_cars[:18]:
         car: Car = car_collection.find({"index": car_index}, {"_id": 0, "index": 1, "make": 1, "model": 1, "city_mpg_for_fuel_type1": 1, "co2_fuel_type1": 1, "cylinders": 1, "drive": 1, "annual_fuel_cost_for_fuel_type1": 1, "fuel_type": 1, "id": 1, "transmission": 1, "vehicle_size_class": 1, "year": 1, "atv_type": 1, "base_model": 1, "image": 1, "price": 1})
         cars.append(car[0])
-
-        if i == 19:
-            break
     
     return cars
-        
 
 
 if __name__ == "__main__":
