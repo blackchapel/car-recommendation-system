362e233d4ff47d1aba6c92ff83089679bb6f9d6d
blackchapel <kc.dark@protonmail.com>
2024-04-11 10:23:51 +0530

    bugs fixed kmeans api wip

--- backend/models/car.py
+++ backend/models/car.py
@@ -4,6 +4,7 @@ class Car(BaseModel):
     index: int
     make: str
     model: str
+    make_model: str
     city_mpg_for_fuel_type1: int
     co2_fuel_type1: int
     cylinders: int
@@ -15,6 +16,21 @@ class Car(BaseModel):
     vehicle_size_class: str
     year: str
     atv_type: str
+    electric_motor: str
     base_model: str
     image: str
+    price: str
+
+class CarRecommendRequest(BaseModel):
+    city_mpg_for_fuel_type1: str
+    co2_fuel_type1: str
+    cylinders: str
+    drive: str
+    annual_fuel_cost_for_fuel_type1: str
+    fuel_type: str
+    transmission: str
+    vehicle_size_class: str
+    year: str
+    atv_type: str
+    electric_motor: str
     price: str
--- backend/routes/car.py
+++ backend/routes/car.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter, Query # type: ignore
-from fastapi.middleware.cors import CORSMiddleware # type: ignore
+import pickle
 import pandas as pd # type: ignore
+import numpy as np # type: ignore
 from gensim.models import Word2Vec # type: ignore
 from sklearn.metrics.pairwise import cosine_similarity # type: ignore
-import numpy as np # type: ignore
-from models.car import Car
+from sklearn.metrics import pairwise_distances # type: ignore
+from models.car import Car, CarRecommendRequest
 from configs.database import car_collection 
 
 router = APIRouter()
@@ -13,16 +14,16 @@ router = APIRouter()
 @router.get("/autocomplete/", response_model=list[Car])
 async def autocomplete_cars(name: str = Query(...)):
     regex = {"$regex": f".*{name}.*", "$options": "i"}
-    cars: Car = car_collection.find({"make_model": regex}, {"_id": 0, "index": 1, "make": 1, "model": 1, "city_mpg_for_fuel_type1": 1, "co2_fuel_type1": 1, "cylinders": 1, "drive": 1, "annual_fuel_cost_for_fuel_type1": 1, "fuel_type": 1, "id": 1, "transmission": 1, "vehicle_size_class": 1, "year": 1, "atv_type": 1, "base_model": 1, "image": 1, "price": 1}).limit(50)
+    cars: Car = car_collection.find({"make_model": regex}, {"_id": 0, "index": 1, "make": 1, "model": 1, "make_model": 1, "city_mpg_for_fuel_type1": 1, "co2_fuel_type1": 1, "cylinders": 1, "drive": 1, "annual_fuel_cost_for_fuel_type1": 1, "fuel_type": 1, "id": 1, "transmission": 1, "vehicle_size_class": 1, "year": 1, "atv_type": 1, "electric_motor": 1, "base_model": 1, "image": 1, "price": 1}).limit(20)
     return cars
 
 
 @router.get("/search-recommendation", response_model=list[Car])
 async def search_recommendation(index: str = Query(...)):
-    data = pd.read_csv('../data/final_car_data.csv')
+    data = pd.read_csv('./data/final_car_data.csv')
     data.astype(str)
     car_features_list = data.iloc[:, :].values.tolist()
-    model = Word2Vec.load('../data/word2vec_model_similarity')
+    model = Word2Vec.load('./data/word2vec_model_similarity')
 
     def calculate_similarity(vec1, vec2):
         return cosine_similarity([vec1], [vec2])[0][0]
@@ -37,7 +38,7 @@ async def search_recommendation(index: str = Query(...)):
         else:
             return None
 
-    car_vectors_data = pd.read_csv('../data/car_vectors_similarity.csv')
+    car_vectors_data = pd.read_csv('./data/car_vectors_similarity.csv')
 
     index = int(index)
     user_preference = car_features_list[index]
@@ -55,7 +56,54 @@ async def search_recommendation(index: str = Query(...)):
 
     cars: list[Car] = []
     for car_index, car_info in sorted_cars[:18]:
-        car: Car = car_collection.find({"index": car_index}, {"_id": 0, "index": 1, "make": 1, "model": 1, "city_mpg_for_fuel_type1": 1, "co2_fuel_type1": 1, "cylinders": 1, "drive": 1, "annual_fuel_cost_for_fuel_type1": 1, "fuel_type": 1, "id": 1, "transmission": 1, "vehicle_size_class": 1, "year": 1, "atv_type": 1, "base_model": 1, "image": 1, "price": 1})
+        car: Car = car_collection.find({"index": car_index}, {"_id": 0, "index": 1, "make": 1, "model": 1, "make_model": 1, "city_mpg_for_fuel_type1": 1, "co2_fuel_type1": 1, "cylinders": 1, "drive": 1, "annual_fuel_cost_for_fuel_type1": 1, "fuel_type": 1, "id": 1, "transmission": 1, "vehicle_size_class": 1, "year": 1, "atv_type": 1, "electric_motor": 1, "base_model": 1, "image": 1, "price": 1})
         cars.append(car[0])
     
     return cars
+
+
+@router.post("/recommend")
+async def recommendation(userPreference: CarRecommendRequest):
+    data_user_preference = {
+        'City Mpg For Fuel Type1': [userPreference.city_mpg_for_fuel_type1],
+        'Co2 Fuel Type1': [userPreference.co2_fuel_type1],
+        'Cylinders': [userPreference.cylinders],
+        'Drive': [userPreference.drive],
+        'Annual Fuel Cost For Fuel Type1': [userPreference.annual_fuel_cost_for_fuel_type1],
+        'Fuel Type': [userPreference.fuel_type],
+        'Transmission': [userPreference.transmission],
+        'Vehicle Size Class': [userPreference.vehicle_size_class],
+        'Year': [userPreference.year],
+        'ATV Type': [userPreference.atv_type],
+        'Electric motor': [userPreference.electric_motor],
+        'Price': [userPreference.price],
+    }
+    df = pd.DataFrame(data_user_preference)
+    # return df.iloc[0]
+
+    with open('./data/kmeans_model.pkl', 'rb') as f:
+        loaded_kmeans = pickle.load(f)
+    centroid_vectors = loaded_kmeans.cluster_centers_
+    model = Word2Vec.load("./data/word2vec_model_cluster")
+
+    def get_word_vector(feature):
+        return model.wv[feature]
+
+    def get_average_vector(features):
+        vectors = [get_word_vector(feature) for feature in features if feature in model.wv]
+        if vectors:
+            return np.mean(vectors, axis=0)
+        else:
+            return None
+
+    user_preferance_vector = get_average_vector(df.iloc[0])
+
+    distances = pairwise_distances([user_preferance_vector], centroid_vectors, metric='euclidean')
+    closest_center_index = np.argmin(distances)
+
+    data_cars = pd.read_csv('./data/car_clusters.csv')
+
+    similiar_cars = data_cars.loc[data_cars['cluster_label'] == closest_center_index]
+
+    print(similiar_cars)
+    print(f"User preference is closest to Cluster {closest_center_index}")
