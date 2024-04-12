a34a4b81845e278f04fafaa31ccf7159734a7878
blackchapel <kc.dark@protonmail.com>
2024-04-12 19:57:41 +0530

    recommendation api

--- backend/models/car.py
+++ backend/models/car.py
@@ -24,13 +24,7 @@ class Car(BaseModel):
 class CarRecommendRequest(BaseModel):
     city_mpg_for_fuel_type1: str
     co2_fuel_type1: str
-    cylinders: str
-    drive: str
-    annual_fuel_cost_for_fuel_type1: str
-    fuel_type: str
-    transmission: str
     vehicle_size_class: str
     year: str
     atv_type: str
-    electric_motor: str
     price: str
--- backend/routes/car.py
+++ backend/routes/car.py
@@ -55,7 +55,7 @@ async def search_recommendation(index: str = Query(...)):
     sorted_cars = sorted(car_similarities.items(), key=lambda x: x[1]['similarity'], reverse=True)
 
     cars: list[Car] = []
-    for car_index, car_info in sorted_cars[:18]:
+    for car_index, car_info in sorted_cars[:9]:
         car: Car = car_collection.find({"index": car_index}, {"_id": 0, "index": 1, "make": 1, "model": 1, "make_model": 1, "city_mpg_for_fuel_type1": 1, "co2_fuel_type1": 1, "cylinders": 1, "drive": 1, "annual_fuel_cost_for_fuel_type1": 1, "fuel_type": 1, "id": 1, "transmission": 1, "vehicle_size_class": 1, "year": 1, "atv_type": 1, "electric_motor": 1, "base_model": 1, "image": 1, "price": 1})
         cars.append(car[0])
     
@@ -67,15 +67,9 @@ async def recommendation(userPreference: CarRecommendRequest):
     data_user_preference = {
         'City Mpg For Fuel Type1': [userPreference.city_mpg_for_fuel_type1],
         'Co2 Fuel Type1': [userPreference.co2_fuel_type1],
-        'Cylinders': [userPreference.cylinders],
-        'Drive': [userPreference.drive],
-        'Annual Fuel Cost For Fuel Type1': [userPreference.annual_fuel_cost_for_fuel_type1],
-        'Fuel Type': [userPreference.fuel_type],
-        'Transmission': [userPreference.transmission],
         'Vehicle Size Class': [userPreference.vehicle_size_class],
         'Year': [userPreference.year],
         'ATV Type': [userPreference.atv_type],
-        'Electric motor': [userPreference.electric_motor],
         'Price': [userPreference.price],
     }
     df = pd.DataFrame(data_user_preference)
@@ -94,16 +88,32 @@ async def recommendation(userPreference: CarRecommendRequest):
             return np.mean(vectors, axis=0)
         else:
             return None
+    
+    user_preference_vector = get_average_vector(df.iloc[0])
 
-    user_preferance_vector = get_average_vector(df.iloc[0])
-
-    distances = pairwise_distances([user_preferance_vector], centroid_vectors, metric='euclidean')
+    distances = pairwise_distances([user_preference_vector], centroid_vectors, metric='euclidean')
     closest_center_index = np.argmin(distances)
 
-    data_cars = pd.read_csv('./data/car_clusters.csv')
+    car_vectors_cluster_label = pd.read_csv('./data/car_vectors_cluster_label.csv')
 
-    similiar_cars = data_cars.loc[data_cars['cluster_label'] == closest_center_index]
+    similiar_cars_vectors = car_vectors_cluster_label.loc[car_vectors_cluster_label['cluster_label'] == closest_center_index]
+    car_data = similiar_cars_vectors.drop(columns=['Unnamed: 0', 'cluster_label'])
 
-    print(similiar_cars)
-    print(f"User preference is closest to Cluster {closest_center_index}")
+    def calculate_similarity(vec1, vec2):
+        return cosine_similarity([vec1], [vec2])[0][0]
+                                  
+    car_similarities = {}
+    for index, row in car_data.iterrows():
+        car_vector = row.tolist()
+        similarity = calculate_similarity(user_preference_vector, car_vector)
+        car_similarities[index] = {'similarity': similarity}
+
+    sorted_cars = sorted(car_similarities.items(), key=lambda x: x[1]['similarity'], reverse=True)
+
+    cars: list[Car] = []
+    for car_index, car_info in sorted_cars[:18]:
+        car: Car = car_collection.find({"index": car_index}, {"_id": 0, "index": 1, "make": 1, "model": 1, "make_model": 1, "city_mpg_for_fuel_type1": 1, "co2_fuel_type1": 1, "cylinders": 1, "drive": 1, "annual_fuel_cost_for_fuel_type1": 1, "fuel_type": 1, "id": 1, "transmission": 1, "vehicle_size_class": 1, "year": 1, "atv_type": 1, "electric_motor": 1, "base_model": 1, "image": 1, "price": 1})
+        cars.append(car[0])
+    
+    return cars
     
